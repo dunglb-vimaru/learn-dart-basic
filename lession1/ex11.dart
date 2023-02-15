d9b71b93c92cda16a4b49e857824f17794b35561
Phạm Thị Quỳnh Anh <115700329+anh0701@users.noreply.github.com>
2023-02-15 08:50:06 +0700

    lession1

--- lession1/ex11.dart
+++ lession1/ex11.dart
@@ -2,6 +2,6 @@ import 'dart:io';
 void main(List<String> args) {
   int? total = int.parse(stdin.readLineSync()!);
   int? people = int.parse(stdin.readLineSync()!);
-  int Formula = total ~/ people;
+  double Formula = total / people;
   print(Formula);
 }
